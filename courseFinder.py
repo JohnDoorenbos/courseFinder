712c7278192e5505da3e972ac115138efcadbcc0
U-John-PC\doorjo01 <doorjo01@John-PC.(none)>
2014-05-01 20:35:55 -0500

    Commiting initial files for Michael

---
+++ courseFinder.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, session, request
+from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import TextField, BooleanField, validators 
+
+
+
+class My_Form(Form): #Is this good for a variable name?
+    name = TextField('Name')#,[validators.Length(min=1,max=25)])
+    department = BooleanField('Spring 2014')#, [validators.Required()])
+    
+    
+
+
+
+    
+app = Flask(__name__)
+Bootstrap(app)
+app.secret_key = "luther"
+@app.route('/courseFinder')
+def forms_page():
+    form = My_Form()
+    print(form.name.data, "   ", form.department.data)
+    print(form.validate())
+    if form.validate_on_submit():
+        
+        return redirect('/success')  #success will have to be the name of our next app.route
+    
+    return render_template("form.html",form = form)
+
+@app.route("/results")
+def results_page():
+    return render_template("results.html")
+if __name__ == "__main__":
+    app.run(debug=True)
