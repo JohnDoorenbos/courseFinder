ed56d91058b5f65949569ec9862e23b320896a62
Michael Moore <mscottmoore16@gmail.com>
2014-05-10 20:51:10 -0500

    experimented with WTForms

--- courseFinder.py
+++ courseFinder.py
@@ -1,14 +1,22 @@
 from config import *
 
 class My_Form(Form): #Is this good for a variable name?
-    name = TextField('Name')#,[validators.Length(min=1,max=25)])
-    department = BooleanField('Spring 2014')#, [validators.Required()])
-    
-@app.route('/courseFinder')
+    name = TextField('Name',[validators.Length(min=4, max=25)])
+    department = BooleanField('Spring 2014', [validators.Required()])
+    submit = SubmitField('Submit')
+
+@app.route('/courseFinder', methods=['POST', 'GET'])
 def forms_page():
-    form = My_Form()
-    print(form.name.data, "   ", form.department.data)
-    print(form.validate())
+    if request.method == 'GET':
+        print 'Request == \'GET\''
+        print 'request.args:', str([key for key in request.args.keys()])
+        form = My_Form(request.args)
+    else:
+        print 'not Request == \'GET\''
+        form = My_Form()
+    print form.name.data, form.department.data
+    print form.validate()
+    print form.validate_on_submit()
     if form.validate_on_submit():
         
         return redirect('/success')  #success will have to be the name of our next app.route
@@ -18,5 +26,10 @@ def forms_page():
 @app.route("/results")
 def results_page():
     return render_template("results.html")
+
+@app.route('/<dept>/<number>')
+def course_page(dept,number):
+    course_id = dept + ' ' + number
+
 if __name__ == "__main__":
     app.run(debug=True)
