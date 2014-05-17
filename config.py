cf1a2d1e7cb91669eb1a3868f84d3ce6e0d46fb6
John A. Doorenbos <john.doorenbos@gmail.com>
2014-05-17 14:02:40 -0500

    fixed syntax error in config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-\from flask import Flask, render_template, session, request, redirect, jsonify, flash
+from flask import Flask, render_template, session, request, redirect, jsonify, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
 from wtforms import IntegerField, TextAreaField, TextField, BooleanField, SubmitField, SelectMultipleField, validators 
