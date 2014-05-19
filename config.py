ba0c7f5f2bbf8fb97427b3f25d74133aa71ab9cd
Michael Moore <mscottmoore16@gmail.com>
2014-05-18 19:13:03 -0500

    switched to postgresql

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ Bootstrap(app)
 app.secret_key = "luther"
 
 db = SQLAlchemy(app)
-dbPath = 'sqlite:////tmp/cf.db'
-#dbPath =  'postgresql+pg8000://flnqsqilzhheoo:mkz5h36UA7R63Om9dPPQ1X4M5i@ec2-54-83-196-217.compute-1.amazonaws.com:5432/dcagbs1vl8cb22'#os.environ['DATABASE_URL']
+#dbPath = 'sqlite:////tmp/cf.db'
+dbPath =  'postgresql+pg8000://flnqsqilzhheoo:mkz5h36UA7R63Om9dPPQ1X4M5i@ec2-54-83-196-217.compute-1.amazonaws.com:5432/dcagbs1vl8cb22'#os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_DATABASE_URI'] =  dbPath
 
 #------------------Defining our Models for the database----------------------------------------------
