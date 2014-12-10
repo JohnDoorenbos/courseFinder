a8f1d4cdea45197ea3a7b8e950d1d9f337ab9951
Michael Moore <mscottmoore16@gmail.com>
2014-12-10 17:46:11 +0800

    tweaked/fixed cursor change in jquery

--- static/script.js
+++ static/script.js
@@ -3,11 +3,11 @@ $(document).ready( function() {
     $('.results .box').hover( function() {
         $(this).css('background-color','#FAFAFA');
 	$(this).css('border-color','#CCCCCC');
-	$(this).css('cursor','hand');
+	$(this).css('cursor','pointer');
     }, function() {
 	$(this).css('background-color','#FFFFFF');
 	$(this).css('border-color','#DCDCDC');
-	$(this).css('cursor','pointer');
+	$(this).css('cursor','default');
     });
 
     $('.results .box').click( function() {
